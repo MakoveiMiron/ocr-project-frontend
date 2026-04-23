e5c5fc8503ba280faadcbb1b0536dac84bd2932a
Makovei Miron <miron.makovei@gmail.com>
2026-04-23 16:15:52 +0200

    asd

--- lib/config.ts
+++ lib/config.ts
@@ -1,3 +1,5 @@
 export const config = {
-  apiBaseUrl: process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:8000/api/v1'
+  apiBaseUrl:
+    process.env.NEXT_PUBLIC_API_BASE_URL ??
+    "https://web-production-3a8489.up.railway.app/api/v1",
 };
